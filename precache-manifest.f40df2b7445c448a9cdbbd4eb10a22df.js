463f45d4d3db7d4727490b22abdd790e2773b90a
Dmitry Podkopaev <dev.elatero@gmail.com>
2020-08-22 15:25:15 +0300

    Updates

---
+++ precache-manifest.f40df2b7445c448a9cdbbd4eb10a22df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba1f6ff8028d379bc1cb086a516e24f8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "785851c3d8de12df00c9",
+    "url": "/monsters-rolodex/static/css/main.47688a10.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "785851c3d8de12df00c9",
+    "url": "/monsters-rolodex/static/js/main.6faebc4f.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
